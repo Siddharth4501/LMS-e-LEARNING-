e123118a79036c7ef3808dfc222a4f1fdc1ff588
Siddharth4501 <siddharth130040@gmail.com>
2024-07-07 20:45:40 +0530

    second commit

---
+++ server/config/dbConnection.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery',false)//if required data or query doesn't exist then it helps handling error by not giving error
+
+const connectionToDB=async ()=>{
+    try{
+        const connection= await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`
+        
+        )
+        if(connection){
+            console.log(`Connected to MongoDB: ${connection.host}`);
+        }
+    } catch(e){
+        console.log(e);
+        process.exit(1);// exit immediately and cancel all process
+    }
+}
+
+export default connectionToDB;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 
 import app from './app.js'
 import {config} from 'dotenv';//considers contents of env file and execute it
+import connectionToDB from './config/dbConnection.js';
 config();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>{
+app.listen(PORT,async()=>{
+    await connectionToDB()
     console.log(`App is running at http:localhost:${PORT}`);
 });
