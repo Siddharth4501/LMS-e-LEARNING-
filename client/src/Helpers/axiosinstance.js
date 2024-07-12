3537bf3ba3ea4a0d0d825e1459ebaf5076f8b0fd
Siddharth4501 <siddharth130040@gmail.com>
2024-07-13 01:38:02 +0530

    created axios instance

---
+++ client/src/Helpers/axiosinstance.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL="http://localhost:5000/api/vi";
+
+const axiosInstance=axios.create();
+
+axiosInstance.defaults.baseURL=BASE_URL;// to make BASE_URL as base url
+
+axiosInstance.defaults.withCredentials=true;//to send or receive data
+
+//axiosInstance.defaults.timeout=5000;
+
+
+export default axiosInstance;
