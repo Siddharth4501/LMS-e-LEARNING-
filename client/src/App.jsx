76a561521a65922607df705d01ca6ef5be529bcf
Siddharth4501 <siddharth130040@gmail.com>
2024-07-15 00:33:12 +0530

    Worked on About Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import {Routes,Route} from 'react-router-dom'
 import NotFound from './pages/NotFound.jsx'
 import Homepage from './pages/Homepage.jsx'
+import About from './pages/AboutPage.jsx'
 function App() {
   
 
@@ -11,6 +12,7 @@ function App() {
       
        <Routes>
           <Route path="/" element={<Homepage/>} />
+          <Route path="/about" element={<About/>} />
           <Route path="*" element={<NotFound />} />
         </Routes> 
     </>
