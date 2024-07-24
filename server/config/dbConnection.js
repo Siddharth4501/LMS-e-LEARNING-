334516f62a0eb90e232ec76ac1d3e399656eefde
Siddharth4501 <siddharth130040@gmail.com>
2024-07-24 23:53:42 +0530

    Fixed errors

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -4,9 +4,8 @@ mongoose.set('strictQuery',false)//if required data or query doesn't exist then 
 
 const connectionToDB=async ()=>{
     try{
-        const connection= await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`
+        const connection= await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
         
-        )
         if(connection){
             console.log(`Connected to MongoDB: ${connection.host}`);
         }
@@ -15,5 +14,8 @@ const connectionToDB=async ()=>{
         process.exit(1);// exit immediately and cancel all process
     }
 }
+
+
+
 
 export default connectionToDB;
