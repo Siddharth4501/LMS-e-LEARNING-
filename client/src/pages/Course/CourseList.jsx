2366e902127b1f34fb444c5928b48b8d0437d1d8
Siddharth4501 <siddharth130040@gmail.com>
2024-07-25 20:36:21 +0530

    Made All Courses Page

---
+++ client/src/pages/Course/CourseList.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CourseCard from "../../components/CourseCard.jsx";
+import HomeLayout from "../../Layouts/HomeLayout.jsx";
+
+import { getAllCourses } from "../../Redux/Slices/CourseSlice.js";
+
+const Courses = () => {
+  const dispatch = useDispatch();
+  const { coursesData } = useSelector((state) => state.course);
+  console.log("mrsfnejgojrioejg",coursesData)
+  useEffect(() => {
+    (async () => {
+      await dispatch(getAllCourses());
+    })();
+  }, []);
+
+  return (
+    <HomeLayout>
+      {/* courses container for displaying the cards */}
+      <div className="min-h-[90vh] pt-12 pl-20 flex flex-col flex-wrap gap-10 text-white bg-gray-600">
+        <h1 className="text-center text-3xl font-semibold">
+          Explore the courses made by{" "}
+          <span className="font-bold text-yellow-500">Industry Experts</span>
+        </h1>
+
+        {/* wrapper for courses card */}
+        <div className="mb-10 flex flex-wrap gap-14">
+          {coursesData?.map((element) => {
+            return <CourseCard key={element._id} data={element} />;
+          })}
+        </div>
+      </div>
+    </HomeLayout>
+  );
+};
+
+export default Courses;
