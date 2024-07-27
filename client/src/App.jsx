1bb130fd4206435a82d10222ae4cec1aa56f4ac7
Siddharth4501 <siddharth130040@gmail.com>
2024-07-27 22:50:48 +0530

    Added Authentication Requirement Check Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Login from './pages/Login.jsx'
 import Contact from './pages/Contact.jsx'
 import CourseList from './pages/Course/CourseList.jsx'
 import Denied from './pages/Denied.jsx'
+import RequireAuth from './components/Auth/RequireAuth.jsx'
 import CourseDescription from './pages/Course/CourseDescription.jsx'
 function App() {
   
@@ -24,9 +25,13 @@ function App() {
           <Route path="/login" element={<Login/>} />
           <Route path="/contact" element={<Contact/>} />
           <Route path="/courses" element={<CourseList/>} />
-          <Route path="/course/description" element={<CourseDescription/>} />
+          <Route element={<RequireAuth allowedRoles={["USER", "ADMIN"]} />}>
+            <Route path="/course/description" element={<CourseDescription />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
         </Routes> 
+
+        
     </>
   )
 }
