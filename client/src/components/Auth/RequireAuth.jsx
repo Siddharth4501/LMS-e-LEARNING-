918fa4e637e1d1010dbe6ae22d9bba1be08c5666
Siddharth4501 <siddharth130040@gmail.com>
2024-07-28 23:42:29 +0530

    Fixed Errors

--- client/src/components/Auth/RequireAuth.jsx
+++ client/src/components/Auth/RequireAuth.jsx
@@ -3,10 +3,13 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const RequireAuth = ({ allowedRoles }) => {
   const { isLoggedIn, role } = useSelector((state) => state.auth);
+  console.log(allowedRoles,"allowedRoles")
+  console.log(role,"role")
   const location = useLocation();
 
   return isLoggedIn && allowedRoles.find((myRole) => myRole === role) ? (
     <Outlet />
+    
   ) : isLoggedIn ? (
     <Navigate to={"/denied"} state={{ from: location }} replace />
   ) : (
